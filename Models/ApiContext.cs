ad126111de3429265fdb59d8f1bb0deace29608f
Hermetheus <allanmolson@gmail.com>
2020-04-27 21:59:02 -0500

    Server Controller HttpRequests

--- Models/ApiContext.cs
+++ Models/ApiContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Advantage.API.Models {
-    public class ApiContext : DbContext {
-        public ApiContext (DbContextOptions<ApiContext> options) : base (options) { }
+namespace Advantage.API.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
         public DbSet<Customer> Customers { get; set; }
 
